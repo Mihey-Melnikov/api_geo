6fcdf0e323fcae336524bd0fdb3a24fa8a7ac89e
Mikhey Melnikov <iordan-1@mail.ru>
2024-04-10 22:20:27 +0500

    modified:   src/geo/router.py 	modified:   src/geo/schemas.py 	new file:   src/osm/main.py

--- src/geo/router.py
+++ src/geo/router.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import update, select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -14,9 +14,16 @@ router = APIRouter(
 
 @router.get("/city/{id}")
 async def get_city_by_id(id: int, session: AsyncSession = Depends(get_async_session)):
-    query = select(city).where(city.c.id == id and city.c.deleted_at is not None)
-    result = await session.execute(query)
-    return result.mappings().all()
+    try:
+        query = select(city).where(city.c.id == id and city.c.deleted_at is not None)
+        result = await session.execute(query)
+        return result.mappings().all()
+    except:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": None,
+        }) 
 
 @router.get("/cities")
 async def get_all_cities(session: AsyncSession = Depends(get_async_session)):
@@ -31,10 +38,6 @@ async def add_city(new_city: CityCreate, session: AsyncSession = Depends(get_asy
     await session.commit()
     return result.mappings().all()
 
-@router.put("/city/{id}")
-async def update_city(updated_rows: CityUpdate, session: AsyncSession = Depends(get_async_session)):
-    pass
-
 @router.delete("/city/{id}")
 async def delete_city(id: int, session: AsyncSession = Depends(get_async_session)):
     query = update(city).where(city.c.id == id).values(deleted_at=datetime.now()).returning(city)
@@ -42,3 +45,30 @@ async def delete_city(id: int, session: AsyncSession = Depends(get_async_session
     await session.commit()
     return result.mappings().all()
 
+"""
+@router.patch("/city/{id}", response_model=CityUpdate)
+async def update_city(id: int, updated_rows: CityUpdate, session: AsyncSession = Depends(get_async_session)) -> City:
+    async with session.begin():
+        query = select(city).options(selectinload(city)).where(city.c.id == id, city.c.deleted_at.is_(None))
+        result = await session.execute(query)
+        city_for_update = result.scalars().one_or_none()
+        if city_for_update is None:
+            raise HTTPException(status_code=404, detail="City not found")
+        update_data = updated_rows.dict(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(city_for_update, key, value)
+        session.add(city_for_update)
+        await session.commit()
+        return city_for_update
+"""
+
+@router.patch("/city/{id}", response_model=CityUpdate)
+async def update_city(id: int, updated_rows: CityUpdate, session: AsyncSession = Depends(get_async_session)):
+    query = select(city).where(city.c.id == id, city.c.deleted_at.is_(None))
+    result = await session.execute(query)
+    city_for_update = result.one_or_none()
+    updated_rows.model_dump(exclude_unset=True)
+    for name, value in updated_rows.model_dump(exclude_unset=True).items():
+        setattr(city_for_update, name, value)
+    await session.commit()
+    return city_for_update
--- src/geo/schemas.py
+++ src/geo/schemas.py
@@ -1,6 +1,20 @@
 from datetime import datetime
+from typing import Optional
 
 from pydantic import BaseModel
+
+class CityBase(BaseModel):
+    id: int
+    region_id: int
+    country_id: int
+    name: str
+    iata: str | None = None
+    timezone: str
+    latitude: float
+    longitude: float
+    osm_id: int
+    osm_type: str
+    need_automatic_update: bool | None = True
 
 class CityCreate(BaseModel):
     region_id: int
@@ -15,13 +29,13 @@ class CityCreate(BaseModel):
     need_automatic_update: bool | None = True
 
 class CityUpdate(BaseModel):
-    region_id: int
-    country_id: int
-    name: str
-    iata: str
-    timezone: str
-    latitude: float
-    longitude: float
-    osm_id: int
-    osm_type: str
-    need_automatic_update: bool
+    region_id: int | None = None
+    country_id: int | None = None
+    name: str | None = None
+    iata: str | None = None
+    timezone: str | None = None
+    latitude: float | None = None
+    longitude: float | None = None
+    osm_id: int | None = None
+    osm_type: str | None = None
+    need_automatic_update: bool | None = None
