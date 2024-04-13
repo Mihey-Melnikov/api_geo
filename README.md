63694d976c59a7a6f1dcbaa70ae125ce9f8ee205
Mikhey Melnikov <iordan-1@mail.ru>
2024-04-14 02:02:03 +0500

    update readme

---
+++ README.md
@@ -0,0 +1,52 @@
+# API Geo
+
+## Быстрый запуск
+
+0. Установите PostgreSQL
+1. Клонируйте репозиторий
+2. Установите все необходимые модули
+
+```python
+pip install -r requirements.txt
+```
+
+3. Создайте локальную БД миграцией
+
+```python
+alembic upgrade gead
+```
+
+4. Запустите проект
+
+```python
+uvicorn src.main:app --reload
+```
+
+## Структура проекта
+
+Основная структура:
+* `migrations/` - миграции
+* `src/` - основной проект
+* `.env` - переменные окружения
+
+Вложенные файлы:
+* `*/models` - описание моделей БД для миграций
+* `*/schemas` - описание схем БД для API
+* `*/router` - роутинг и основной код API
+
+## FAQ
+
+Создание новой миграции
+
+```python
+alembic revision --autogenerate -m "message"
+```
+
+Работа с виртаульным окружением. Запуск:
+```python
+venv\Scripts\activate.ps1
+```
+Выход:
+```python
+deactivate
+```
--- src/city/models.py
+++ src/city/models.py
@@ -21,20 +21,6 @@ city = Table("city", metadata,
     Column("need_automatic_update", Boolean, default=True),
 )
 
-metro = Table("metro", metadata,
-    Column("id", Integer, primary_key=True, index=True),
-    Column("city_id", Integer, ForeignKey(city.c.id), nullable=False),
-    Column("station_name", String, nullable=False),
-    Column("line_name", String, nullable=False),
-    Column("latitude", Float, nullable=False),
-    Column("longitude", Float, nullable=False),
-    Column("osm_id", Integer, nullable=False),
-    Column("osm_type", String(1), nullable=False),
-    Column("last_updated_at", TIMESTAMP, default=datetime.now, onupdate=datetime.now, nullable=False),
-    Column("deleted_at", TIMESTAMP, default=None, nullable=True),
-    Column("need_automatic_update", Boolean, default=True),
-)
-
 translation_language = Table("translation_language", metadata,
     Column("language_iso639", String, primary_key=True, index=True),
     Column("description", String, nullable=False),
